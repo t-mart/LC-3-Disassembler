5dad1ea1092434295c13de87a97282cfaad6de55
= <=>
2011-09-30 22:36:58 -0700

    write add logic

--- disassemble.rb
+++ disassemble.rb
@@ -1,7 +1,7 @@
 #Ruby LC-3 Disassembler
 #by Tim Martin
 
-#everything is written in this method. because this code is serving simply as psuedo-code for the final version in assembly itself, im going to spare the ruby-isms and code more like you would in asm.
+#everything is written in this method. because this code is serving simply as psuedo-code for the final version in assembly itself, IM GOING TO SPARE THE RUBYISMS AND CODE MORE LIKE ASM.
 def disassemble memory	
 	#we're going to be parsing 16-bit instructions. these masks will allow us to get meaningful data out of out of those 16 bits
 	#to use: (instruction & mask) >> x, where x is the index of the least-significant bit of of the mask
@@ -30,9 +30,9 @@ def disassemble memory
 
 	#offset masks
 	#offset masks are always the least significant x bits
-	offset_9_mask = 0x1FF
-	offset_11_mask = 0x7FF
-	offset_6_mask = 0x3f
+	offset9_mask = 0x1FF
+	offset11_mask = 0x7FF
+	offset6_mask = 0x3f
 
 	#imm5 mask
 	#always lsb
@@ -95,24 +95,122 @@ def disassemble memory
 	cc_string = "cc"
 	comma_string = ","
 	halt_string = "HALT"
+	space_string = " "
+	pc_string = "PC"
+	r_string = "R" #as in register, R1, R2, ...
+	colon_string = ":"
+
 	
 	#a more asm way of saying index counter
 	instruction_ptr = 0x0
 
-	#give us some table headers
-	puts "addr\tinstruction"
-
 	while memory[instruction_ptr] - 0xFFFF != 0
 		this_instruction = memory[instruction_ptr]
 		
-		opcode = (this_instruction & opcode_mask) >> 12
+		this_opcode = (this_instruction & opcode_mask) >> 12
+		
+		#ADD
+		#example: R0 = R0 + R5
+		#example: R0 = R3 + 17
+		if this_opcode - add_code == 0
+			print add_string
+			print colon_string
+			print space_string
+			print r_string
+			reg = (this_instruction & reg1_mask) >> 9
+			print reg
+			print space_string
+			print equal_sign_string
+			print space_string
+			print r_string
+			reg = (this_instruction & reg2_mask) >> 6
+			print reg
+			print space_string
+			print addition_sign_string
+			print space_string
+			flag = (this_instruction & operand2_flag_mask) >> 5
+			if flag == 0
+				print r_string
+				reg = (this_instruction & reg3_mask)
+				print reg
+			end
+			if flag == 1
+				imm5 = (this_instruction & imm5_mask)
+				print imm5
+			end
 
-		puts "0x#{instruction_ptr.to_s(16).rjust(4,"0")}\t#{opcode}"
+		end
+		
+		#AND
+		if this_opcode - and_code == 0
+			this_opcode_string = and_code
+		end
+
+		if this_opcode - br_code == 0
+			this_opcode_string = br_string
+		end
+
+		if this_opcode - br_code == 0
+			this_opcode_string = br_string
+		end
+
+		if this_opcode - br_code == 0
+			this_opcode_string = br_string
+		end
+
+		if this_opcode - jmp_code == 0
+			this_opcode_string = jmp_string
+		end
+
+		if this_opcode - jsr_code == 0
+			this_opcode_string = jsr_string
+		end
+
+		if this_opcode - ld_code == 0
+			this_opcode_string = ld_string
+		end
+
+		if this_opcode - ldi_code == 0
+			this_opcode_string = ldi_string
+		end
+
+		if this_opcode - ldr_code == 0
+			this_opcode_string = ldr_string
+		end
+
+		if this_opcode - lea_code == 0
+			this_opcode_string = lea_string
+		end
+
+		if this_opcode - not_code == 0
+			this_opcode_string = not_string
+		end
+
+		if this_opcode - st_code == 0
+			this_opcode_string = st_string
+		end
+
+		if this_opcode - sti_code == 0
+			this_opcode_string = sti_string
+		end
+
+		if this_opcode - str_code == 0
+			this_opcode_string = str_string
+		end
+
+		if this_opcode - trap_code == 0
+			this_opcode_string = trap_string
+		end
+
+		#puts "0x#{instruction_ptr.to_s(16).rjust(4,"0")}\t#{opcode}"
+
+		print "\n"
 
 		instruction_ptr += 1
 	end
 end
 
-test_memory = (0x0..0xF).collect { |i| i << 12 }
+test_memory = [0x100F, 0x102F]
+#test_memory = (0x0..0xF).collect { |i| i << 12 }
 
 disassemble test_memory + [0xFFFF]
