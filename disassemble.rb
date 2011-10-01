710cb6590ed0eed2d054d8f554d440ec3ac3b1b3
= <=>
2011-09-30 20:43:48 -0700

    comment on masks, rename some masks

--- disassemble.rb
+++ disassemble.rb
@@ -1,11 +1,19 @@
+#Ruby LC-3 Disassembler
+#by Tim Martin
+
+#everything is written in this method. because this code is serving simply as psuedo-code for the final version in assembly itself, im going to spare the ruby-isms and code more like you would in asm.
 def disassemble memory	
+	#we're going to be parsing 16-bit instructions. these masks will allow us to get meaningful data out of out of those 16 bits
+	#to use: (instruction & mask) >> x, where x is the index of the least-significant bit of of the mask
+	#e.g (0x00011011000001100 & opcode mask) >> 12 = 0001
+
 	#opcode mask
 	opcode_mask = 0xF000
 
 	#destination/base and source register masks
-	11to9_mask = 0xE00
-	8to6_mask = 0x1C0
-	2to0_mask = 0x7
+	reg1_mask = 0xE00 #the register found at bits 11 through 9
+	reg2_mask = 0x1C0 #the register found at bit 8 through 6
+	reg3_mask = 0x7 #the reigster found at bits 2 through 0
 
 	#condition code masks
 	br_n_mask = 0x800
@@ -16,15 +24,14 @@ def disassemble memory
 	#if 0, using 2 registers. if 1, using a register and a imm5
 	operand2_flag_mask = 0x20
 
-	#PC offset masks
-	pc_offset_9_mask = 0x1FF
-	pc_offset_11_mask = 0x7FF
+	#offset masks
+	#offset masks are always the least significant x bits
+	offset_9_mask = 0x1FF
+	offset_11_mask = 0x7FF
+	offset_6_mask = 0x3f
 
 	#imm5 mask
 	imm5_mask = 0x1F
-
-	#ldr/str offset mask
-	offset6_mask = 0x3F
 
 	#trap vector mask
 	trap_vector_mask = 0xFF
