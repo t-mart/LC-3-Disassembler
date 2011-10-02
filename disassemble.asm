aca55904faf572e987a9077d2004b7d71ece6dd6
Tim Martin <teimu.tm@gmail.com>
2011-10-02 03:23:06 -0700

    try to do add, but don't actually get anywhere

--- disassemble.asm
+++ disassemble.asm
@@ -11,10 +11,6 @@ POINTER .fill xFE00
 
 DISASSEMBLE:
 
-;everything is written in this method. because this code is serving simply as
-;psuedo-code for the final version in assembly itself, IM GOING TO SPARE THE
-;RUBYISMS AND CODE MORE LIKE ASM.
-def disassemble memory  
   ;we're going to be parsing 16-bit instructions. these masks will allow us to
   ;get meaningful data out of out of those 16 bits
   ;to use: (instruction & mask) >> x, where x is the index of the
@@ -22,91 +18,101 @@ def disassemble memory
   ;e.g (0x00011011000001100 & opcode mask) >> 12 = 0001
 
   ;opcode mask
-  opcode_mask = 0xF000
+  opcode_mask .fill xF000
 
   ;destination/base and source register masks
-  reg1_mask = 0xE00 ;the register found at bits 11 through 9
-  reg2_mask = 0x1C0 ;the register found at bit 8 through 6
-  reg3_mask = 0x7 ;the register found at bits 2 through 0
+  reg1_mask .fill xE00 ;the register found at bits 11 through 9
+  reg2_mask .fill x1C0 ;the register found at bit 8 through 6
+  reg3_mask .fill x7 ;the register found at bits 2 through 0
 
   ;condition code masks
-  br_cc_mask = 0xE00 ;the whole kitten kaboodle, >> 9
-  br_n_mask = 0x800 ; >> 11
-  br_z_mask = 0x400 ; >> 10
-  br_p_mask = 0x200 ; >> 9
+  br_cc_mask .fill xE00 ;the whole kitten kaboodle, >> 9
+  br_n_mask .fill x800 ; >> 11
+  br_z_mask .fill x400 ; >> 10
+  br_p_mask .fill x200 ; >> 9
 
   ;imm5 vs source register flag mask
   ;if 0, using 2 registers. if 1, using a register and a imm5
-  operand2_flag_mask = 0x20 ; >> 5
+  operand2_flag_mask .fill x20 ; >> 5
 
   ;jsr vs jsrr flag mask
   ;if 0, jsrr. if 1, jsr
-  jsr_flag_mask = 0x800 ; >> 7
+  jsr_flag_mask .fill x800 ; >> 7
 
   ;offset masks
   ;offset masks are always the least significant x bits
-  offset9_mask = 0x1FF
-  offset11_mask = 0x7FF
-  offset6_mask = 0x3f
+  offset9_mask .fill x1FF
+  offset11_mask .fill x7FF
+  offset6_mask .fill x3f
 
   ;imm5 mask
   ;always lsb
-  imm5_mask = 0x1F
+  imm5_mask .fill x1F
 
   ;trap vector mask
   ;always lsb
-  trap_vector_mask = 0xFF
+  trap_vector_mask .fill xFF
 
   
   ;opcodes
-  add_code = 0x1
-  and_code = 0x5
-  br_code = 0x0
-  jmp_code = 0xC
-  jsr_code = 0x4
-  ld_code = 0x2
-  ldi_code = 0xA
-  ldr_code = 0x6
-  lea_code = 0xE
-  not_code = 0x9
-  rti_code = 0x8
-  st_code = 0x3
-  sti_code = 0xB
-  str_code = 0x7
-  ;trap_code = 0xF
-  halt_code = 0xF025 ; we only have 1 trap (0xF---), and thats halt
+  add_code .fill x1
+  and_code .fill x5
+  br_code .fill x0
+  jmp_code .fill xC
+  jsr_code .fill x4
+  ld_code .fill x2
+  ldi_code .fill xA
+  ldr_code .fill x6
+  lea_code .fill xE
+  not_code .fill x9
+  rti_code .fill x8
+  st_code .fill x3
+  sti_code .fill xB
+  str_code .fill x7
+  ;trap_code .fill xF
+  halt_code .fill xF025 ; we only have 1 trap (xF---), and thats halt
 
+  ;sentinel value
+  sentinel .fill xFFFF
+
+  ;constants
+  zero .fill x0
+  one .fill x1
+  five .fill x5
+  six .fill x6
+  nine .fill x9
+  ten .fill xA
+  eleven .fill xB
+  twelve .fill xC
 
   ;opcode strings
-  add_string = "ADD"
-  and_string = "AND"
-  br_string = "BR"
-  jmp_string = "JMP"
-  jsr_string = "JSR"
-  jsrr_string = "JSRR"
-  ld_string = "LD"
-  ldi_string = "LDI"
-  ldr_string = "LDR"
-  lea_string = "LEA"
-  not_string = "NOT"
-  nop_string = "NOP"
-  ret_string = "RET"
-  rti_string = "RTI"
-  st_string = "ST"
-  sti_string = "STI"
-  str_string = "STR"
-  trap_string = "TRAP"
+  add_string .stringz "ADD "
+  and_string .stringz "AND "
+  br_string .stringz "BR "
+  jmp_string .stringz "JMP "
+  jsr_string .stringz "JSR "
+  jsrr_string .stringz "JSRR "
+  ld_string .stringz "LD "
+  ldi_string .stringz "LDI "
+  ldr_string .stringz "LDR "
+  lea_string .stringz "LEA "
+  not_string .stringz "NOT "
+  nop_string .stringz "NOP "
+  ret_string .stringz "RET "
+  rti_string .stringz "RTI "
+  st_string .stringz "ST "
+  sti_string .stringz "STI "
+  str_string .stringz "STR "
+  trap_string .stringz "TRAP "
 
   ;other strings
-  n_string = "N"
-  z_string = "Z"
-  p_string = "P"
-  comma_string = ","
-  halt_string = "HALT"
-  space_string = " "
-  r_string = "R" ;as in register, R1, R2, ...
-  error_string = "ERROR"
-  hash_string = ";"
+  n_string .stringz "N"
+  z_string .stringz "Z"
+  p_string .stringz "P"
+  comma_string .stringz ", "
+  halt_string .stringz "HALT"
+  r_string .stringz "R" ;as in register, R1, R2, ...
+  error_string .stringz "ERROR"
   
   ;sign extension addends
   ;we can encounter 2's complement numbers that are negative in x number of
@@ -133,64 +139,103 @@ def disassemble memory
   ;NOTE: this is a system-dependent implementation. on this dev machine, the
   ;integers we get by default are 32 bits. on lc3, its 16, and it could be
   ;anything else for other machines. proceed with caution.
-;  bit5_extender = 0xFFFFFFE0
-;  bit6_extender = 0xFFFFFFC0
-;  bit9_extender = 0xFFFFFE00
-;  bit11_extender = 0xFFFFF800
+;  bit5_extender = xFFFFFFE0
+;  bit6_extender = xFFFFFFC0
+;  bit9_extender = xFFFFFE00
+;  bit11_extender = xFFFFF800
 
-  
-  ;a more asm way of saying index counter
-  instruction_ptr = 0x0
+  ;Register conventions
+  ;R0
+  ;R1
+  ;R2
+  ;R3
+  ;R4 - this opcode
+  ;R5 - this instruction
+  ;R6 - off limits
+  ;R7 - off limits
 
-  while memory[instruction_ptr] - 0xFFFF != 0
-    this_instruction = memory[instruction_ptr]
-    
-    this_opcode = (this_instruction & opcode_mask) >> 12
+  instruction_ptr .fill x5000
+
+  ldi R5, instruction_ptr
+
+  ld R0, sentinel
+  not R0, R0
+  add R0, R0, #1
+  add R0, R0, R5
+
+  brz STOP_DISASSEMBLY
+
+  DISASSEMBLE_NEXT
+    ld R1, opcode_mask
+    and R1, R5, R1
+    ld R0, twelve
+    jsr RSHIFT
+    add r4, r0, #0
 
     ;we'll force the opcode code to turn this on
     ;if we get down to the bottom, and this hasn't been set, we've encountered
     ;one of the no-no opcodes for this homework, such as RTI, (reserved
     ;opcode), or traps other than HALT. in that case, write ERROR
-    valid_instruction = 0
+    valid_instruction .fill x0
     
     ;ADD
     ;example: ADD R0, R0, R5
     ;example: ADD R0, R3, ;17
-    if this_opcode - add_code == 0
-      print add_string
-      print space_string
+    ADD_LOGIC
+    ld R0, add_code
+    not r0, r0
+    add r0, r0, #1
+    add r0, r0, r4
+    brnp AND_LOGIC
+      ld R0, add_string
+      puts
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ld r0, r_string
+      puts
+      ld r0, nine
+      ld r1, reg1_mask
+      and r1, r5, r1
+      jsr RSHIFT
+      puts
+      ld r0, comma_string
+      puts
 
-      print r_string
-      reg = (this_instruction & reg2_mask) >> 6
-      print reg
-      print comma_string
-      print space_string
+      ld r0, r_string
+      puts
+      ld r0, six
+      ld r1, reg2_mask
+      and r1, r5, r1
+      jsr RSHIFT
+      puts
+      ld r0, comma_string
+      puts
       
       ;immediate more or source register 2 mode?
-      flag = (this_instruction & operand2_flag_mask) >> 5
-      if flag == 0
-        print r_string
-        reg = (this_instruction & reg3_mask)
-        print reg
-      end
-      if flag == 1
-        imm5 = (this_instruction & imm5_mask)
-        print hash_string
-        print imm5
-      end
+      ld r0, five
+      ld r1, operand2_flag_mask
+      and r1, r5, r1
+      JSR RSHIFT
+      ADD R0, R0, #0
+      BRP ADD_IMM5
+        LD r0, r_string
+        puts
+        ld r0, reg3_mask
+        and r0, r0, r5
+        puts
+        br FINISH_ADD
+      ADD_IMM5
+        ld r0, imm5_mask
+        and r0, r0, r5
+        JSR PRINT_NUM
 
-      valid_instruction = 1
-    end
+      FINISH_ADD
+      LD R0, ONE
+      ST R0, valid_instruction
     
     ;AND
     ;example: AND R0, R0, R5
     ;example: AND R0, R3, ;17
+    AND_LOGIC
     if this_opcode - and_code == 0
       print and_string
       print space_string
@@ -489,39 +534,8 @@ def disassemble memory
 end
 
 test_memory = [
-  0x100F,
-  0x102F,
-  0x500F,
-  0x502F,
-  0x0E00,
-  0x027C,
-  0x0463,
-  0x06E0,
-  0x090D,
-  0x0AAC,
-  0x0CFD,
-  0x0E64,
-  0x00B2,
-  0xC180,
-  0xC1C0,
-  0x49C0,
-  0x41C0,
-  0x284F,
-  0xA84D,
-  0x684D,
-  0xEA0D,
-  0x9A3F,
-  0x8000,
-  0x86AF,
-  0x3A34,
-  0xBA34,
-  0x7A34,
-  0xF025,
-  0xF024
 ]
-;test_memory = (0x0..0xF).collect { |i| i << 12 }
-
-disassemble test_memory + [0xFFFF]
+;test_memory = (x0..xF).collect { |i| i << 12 }
   
 ;======DO NOT EDIT THIS SECTION=================================================
 
@@ -615,10 +629,35 @@ PRINT_NUM:
 
 ;Instructions to disassemble.  Put things here to test them.
 .orig x5000
-  ADD R0, R0, R0
-  BRNZP 0
-  HALT
-  .fill xD000
+  .fill x100F
+  .fill x102F
+  .fill x500F
+  .fill x502F
+  .fill x0E00
+  .fill x027C
+  .fill x0463
+  .fill x06E0
+  .fill x090D
+  .fill x0AAC
+  .fill x0CFD
+  .fill x0E64
+  .fill x00B2
+  .fill xC180
+  .fill xC1C0
+  .fill x49C0
+  .fill x41C0
+  .fill x284F
+  .fill xA84D
+  .fill x684D
+  .fill xEA0D
+  .fill x9A3F
+  .fill x8000
+  .fill x86AF
+  .fill x3A34
+  .fill xBA34
+  .fill x7A34
+  .fill xF025
+  .fill xF024
   .fill xFFFF
 .end
 
