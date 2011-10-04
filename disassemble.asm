ac35bfa9a6539daa2b94a1cdfd9bf00c1809271e
reverebeer <teimu.tm@gmail.com>
2011-10-03 19:33:36 -0700

    comment out all the ruby

--- disassemble.asm
+++ disassemble.asm
@@ -21,7 +21,6 @@ DISASSEMBLE:
   ;R6 - off limits
   ;R7 - off limits
 
-  instruction_ptr .fill x5000
 
   ldi R5, instruction_ptr
 
@@ -38,12 +37,6 @@ DISASSEMBLE:
     ld R0, twelve
     jsr RSHIFT
     add r4, r0, #0
-
-    ;we'll force the opcode code to turn this on
-    ;if we get down to the bottom, and this hasn't been set, we've encountered
-    ;one of the no-no opcodes for this homework, such as RTI, (reserved
-    ;opcode), or traps other than HALT. in that case, write ERROR
-    valid_instruction .fill x0
     
     ;ADD
     ;example: ADD R0, R0, R5
@@ -103,303 +96,308 @@ DISASSEMBLE:
     ;example: AND R0, R0, R5
     ;example: AND R0, R3, ;17
     AND_LOGIC
-    if this_opcode - and_code == 0
-      print and_string
-      print space_string
+    ;if this_opcode - and_code == 0
+      ;print and_string
+      ;print space_string
       
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg2_mask) >> 6
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg2_mask) >> 6
+      ;print reg
+      ;print comma_string
+      ;print space_string
       
-      flag = (this_instruction & operand2_flag_mask) >> 5
-      if flag == 0
-        print r_string
-        reg = (this_instruction & reg3_mask)
-        print reg
-      end
-      if flag == 1
-        imm5 = (this_instruction & imm5_mask)
-        print hash_string
-        print imm5
-      end
+      ;flag = (this_instruction & operand2_flag_mask) >> 5
+      ;if flag == 0
+        ;print r_string
+        ;reg = (this_instruction & reg3_mask)
+        ;print reg
+      ;end
+      ;if flag == 1
+        ;imm5 = (this_instruction & imm5_mask)
+        ;print hash_string
+        ;print imm5
+      ;end
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
   
-    ;BR and NOP
-    ;example: BR NZP ;4
-    ;example: NOP
-    if this_opcode - br_code == 0
-      cc = (this_instruction & br_cc_mask) >> 9
-      if cc == 0
-        print nop_string
-      else
-        print br_string
+    ;;BR and NOP
+    ;;example: BR NZP ;4
+    ;;example: NOP
+    ;if this_opcode - br_code == 0
+      ;cc = (this_instruction & br_cc_mask) >> 9
+      ;if cc == 0
+        ;print nop_string
+      ;else
+        ;print br_string
       
-        ;n set?
-        n = (this_instruction & br_n_mask) >> 11
-        if n > 0
-          print n_string
-        end
+        ;;n set?
+        ;n = (this_instruction & br_n_mask) >> 11
+        ;if n > 0
+          ;print n_string
+        ;end
       
-        ;z set?
-        z = (this_instruction & br_z_mask) >> 10
-        if z > 0
-          print z_string
-        end
+        ;;z set?
+        ;z = (this_instruction & br_z_mask) >> 10
+        ;if z > 0
+          ;print z_string
+        ;end
       
-        ;p set?
-        p = (this_instruction & br_p_mask) >> 9
-        if p > 0
-          print p_string
-        end
+        ;;p set?
+        ;p = (this_instruction & br_p_mask) >> 9
+        ;if p > 0
+          ;print p_string
+        ;end
       
-        print space_string
-        offset = (this_instruction & offset9_mask)
-        print hash_string
-        print offset
-      end
+        ;print space_string
+        ;offset = (this_instruction & offset9_mask)
+        ;print hash_string
+        ;print offset
+      ;end
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;JMP and RET
-    ;example: JMP R5
-    ;example: RET
-    if this_opcode - jmp_code == 0
-      ;ret and jmp share the same opcode
-      reg = (this_instruction & reg2_mask) >> 6
-      if reg - 7 == 0
-        print ret_string
-      else
-        print jmp_string
-        print space_string
+    ;;JMP and RET
+    ;;example: JMP R5
+    ;;example: RET
+    ;if this_opcode - jmp_code == 0
+      ;;ret and jmp share the same opcode
+      ;reg = (this_instruction & reg2_mask) >> 6
+      ;if reg - 7 == 0
+        ;print ret_string
+      ;else
+        ;print jmp_string
+        ;print space_string
 
-        print r_string
-        print reg
-      end
+        ;print r_string
+        ;print reg
+      ;end
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;JSR and JSRR
-    ;example: JSR ;123
-    ;example: JSRR R3
-    if this_opcode - jsr_code == 0
-      ;jsr and jsrr share the same opcode
-      jsr_flag = (this_instruction & jsr_flag_mask) >> 11
-      if jsr_flag > 0
-        offset = (this_instruction & offset11_mask)
-        print jsr_string
-        print space_string
+    ;;JSR and JSRR
+    ;;example: JSR ;123
+    ;;example: JSRR R3
+    ;if this_opcode - jsr_code == 0
+      ;;jsr and jsrr share the same opcode
+      ;jsr_flag = (this_instruction & jsr_flag_mask) >> 11
+      ;if jsr_flag > 0
+        ;offset = (this_instruction & offset11_mask)
+        ;print jsr_string
+        ;print space_string
 
-        print hash_string
-        print offset
-      else
-        print jsrr_string
-        print space_string
+        ;print hash_string
+        ;print offset
+      ;else
+        ;print jsrr_string
+        ;print space_string
 
-        print r_string
-        reg = (this_instruction & reg2_mask) >> 6
-        print reg
-      end
+        ;print r_string
+        ;reg = (this_instruction & reg2_mask) >> 6
+        ;print reg
+      ;end
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;LD
-    ;example: LD R3, ;5
-    if this_opcode - ld_code == 0
-      print ld_string
-      print space_string
+    ;;LD
+    ;;example: LD R3, ;5
+    ;if this_opcode - ld_code == 0
+      ;print ld_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset9_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset9_mask)
+      ;print hash_string
+      ;print offset
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;LDI
-    ;example: LDI R2, ;22
-    if this_opcode - ldi_code == 0
-      print ldi_string
-      print space_string
+    ;;LDI
+    ;;example: LDI R2, ;22
+    ;if this_opcode - ldi_code == 0
+      ;print ldi_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset9_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset9_mask)
+      ;print hash_string
+      ;print offset
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;LDR
-    ;example: LDR R0, R4, ;5
-    if this_opcode - ldr_code == 0
-      print ldr_string
-      print space_string
+    ;;LDR
+    ;;example: LDR R0, R4, ;5
+    ;if this_opcode - ldr_code == 0
+      ;print ldr_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg2_mask) >> 6
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg2_mask) >> 6
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset6_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset6_mask)
+      ;print hash_string
+      ;print offset
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;LEA
-    ;example: LEA R3, ;2
-    if this_opcode - lea_code == 0
-      print lea_string
-      print space_string
+    ;;LEA
+    ;;example: LEA R3, ;2
+    ;if this_opcode - lea_code == 0
+      ;print lea_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset9_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset9_mask)
+      ;print hash_string
+      ;print offset
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;NOT
-    ;example: NOT R0, R0
-    if this_opcode - not_code == 0
-      print not_string
-      print space_string
+    ;;NOT
+    ;;example: NOT R0, R0
+    ;if this_opcode - not_code == 0
+      ;print not_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg2_mask) >> 6
-      print reg
+      ;print r_string
+      ;reg = (this_instruction & reg2_mask) >> 6
+      ;print reg
       
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;ST
-    ;example: ST R1, ;46
-    if this_opcode - st_code == 0
-      print st_string
-      print space_string
+    ;;ST
+    ;;example: ST R1, ;46
+    ;if this_opcode - st_code == 0
+      ;print st_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset9_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset9_mask)
+      ;print hash_string
+      ;print offset
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;STI
-    ;example: STI R0, ;128
-    if this_opcode - sti_code == 0
-      print sti_string
-      print space_string
+    ;;STI
+    ;;example: STI R0, ;128
+    ;if this_opcode - sti_code == 0
+      ;print sti_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset9_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset9_mask)
+      ;print hash_string
+      ;print offset
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;STR
-    ;example STR R0, R1, ;3
-    if this_opcode - str_code == 0
-      print str_string
-      print space_string
+    ;;STR
+    ;;example STR R0, R1, ;3
+    ;if this_opcode - str_code == 0
+      ;print str_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg1_mask) >> 9
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg1_mask) >> 9
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      print r_string
-      reg = (this_instruction & reg2_mask) >> 6
-      print reg
-      print comma_string
-      print space_string
+      ;print r_string
+      ;reg = (this_instruction & reg2_mask) >> 6
+      ;print reg
+      ;print comma_string
+      ;print space_string
 
-      offset = (this_instruction & offset6_mask)
-      print hash_string
-      print offset
+      ;offset = (this_instruction & offset6_mask)
+      ;print hash_string
+      ;print offset
 
-      valid_instruction = 1
+      ;valid_instruction = 1
 
-    end
+    ;end
 
-    ;HALT
-    ;example: HALT
-    if this_instruction - halt_code == 0
-      print halt_string
+    ;;HALT
+    ;;example: HALT
+    ;if this_instruction - halt_code == 0
+      ;print halt_string
 
-      valid_instruction = 1
-    end
+      ;valid_instruction = 1
+    ;end
 
-    ;ERROR (for all not accepted instructions)
-    ;example: ERROR
-    if valid_instruction == 0
-      print error_string
-    end
+    ;;ERROR (for all not accepted instructions)
+    ;;example: ERROR
+    ;if valid_instruction == 0
+      ;print error_string
+    ;end
 
-    print "\n"
+    ;print "\n"
 
-    instruction_ptr += 1
-  end
-end
+    ;instruction_ptr += 1
 
+  instruction_ptr .fill x5000
+
+  ;we'll force the opcode code to turn this on
+  ;if we get down to the bottom, and this hasn't been set, we've encountered
+  ;one of the no-no opcodes for this homework, such as RTI, (reserved
+  ;opcode), or traps other than HALT. in that case, write ERROR
+  valid_instruction .fill x0
 
   ;we're going to be parsing 16-bit instructions. these masks will allow us to
   ;get meaningful data out of out of those 16 bits
@@ -503,10 +501,8 @@ end
   halt_string .stringz "HALT"
   r_string .stringz "R" ;as in register, R1, R2, ...
   error_string .stringz "ERROR"
-test_memory = [
-]
-;test_memory = (x0..xF).collect { |i| i << 12 }
   
+  .end
 ;======DO NOT EDIT THIS SECTION=================================================
 
 ;; Preconditions:
